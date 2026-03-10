560db968455a2627fb519b9b771a9ee027a6b3cb
768650-cell <768650@student.dusd.net>
2026-03-10 14:45:39 -0700

    Scripting Project
    I made 3 new scripts today.

---
+++ P7SinghScriptingProject/Assets/Script/DualAxisExample.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxisExample : MonoBehaviour
+{
+   
+    public float hRange;
+    public float vRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float xPos = h * hRange;
+        float zPos = v * vRange;
+
+        transform.position = new Vector3(xPos, 0, zPos);
+       Debug.Log("Horizontal: " + h.ToString("F2"));
+       Debug.Log("Vertical: " + v.ToString("F2"));
+    }
+}
