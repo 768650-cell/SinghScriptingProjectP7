37d3eabfedf8a99521f7513d3c578e90fd023c35
768650-cell <768650@student.dusd.net>
2026-03-18 14:44:07 -0700

    Scripting project
    I finish 2 scripting project today.

---
+++ P7SinghScriptingProject/Assets/Script/GetComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComponent : MonoBehaviour
+{
+
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
